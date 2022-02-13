cd97e7ea9958c8d11eecc1ac19d07153b2d0aa0e
Mykola Pidhornyi <mykola436@gmail.com>
2022-02-13 17:37:56 +0200

    change js and end of work

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,17 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('body'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', () => {toggleModal(); toggleBodyOverflow('hidden')});
+  refs.closeModalBtn.addEventListener('click', () => {toggleModal(); toggleBodyOverflow('visible')});
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+  }
+
+  function toggleBodyOverflow(value) {
+    refs.body.style.overflow = value;
   }
 })();
