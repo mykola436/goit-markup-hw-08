4bffc00ad9429e514732d132510a62b623ebfe0b
Mykola Pidhornyi <mykola436@gmail.com>
2022-02-13 16:38:12 +0200

    change js

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,17 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     Menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.openMenuBtn.addEventListener('click', () => {toggleMenu(); toggleBodyOverflow('hidden')});
+  refs.closeMenuBtn.addEventListener('click', () => {toggleMenu(); toggleBodyOverflow('visible')});
 
   function toggleMenu() {
     refs.Menu.classList.toggle('is-hidden');
+  }
+
+  function toggleBodyOverflow(value) {
+    refs.body.style.overflow = value;
   }
 })();
